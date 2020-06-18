f055252ae79127a36f5edf8a82732bc404954bfd
bergeramit <bergeramit@gmail.com>
2020-06-19 00:52:08 +0300

    betterPOC version with better file naming and extraction method

--- extract_notes_from_kindle.py
+++ extract_notes_from_kindle.py
@@ -1,75 +1,72 @@
 """Usage: extract_notes_from_kindle.py <kindle_clipping_file> <output_dir>
 
 Options:
-	kindle_clipping_file     The My_Clippings.txt from kindle file system
+    kindle_clipping_file     The My_Clippings.txt from kindle file system
+    output_dir               Where to save the notes from the books
 """
-import docopt
 import os
 import itertools
-from collections import namedtuple
-
-class KindleClip:
-	def __init__(self, book_name, book_location, date_added, note_data):
-		self.book_name = book_name
-		self.book_location = book_location
-		self.date_added = date_added
-		self.note_data = note_data
+import string
+import docopt
 
 
 class KindleClippingsGenerator:
-	KINDLE_END_NOTE = "=========="
+    KINDLE_END_NOTE = "=========="
 
-	def __init__(self, clippings_file_path):
-		self.clippings_file_handler = open(clippings_file_path, "r")
+    def __init__(self, clippings_file_path):
+        self.handler = open(clippings_file_path, "rb")
 
-	def __iter__(self):
-		return self
+    def __iter__(self):
+        return self
 
-	def __next__(self):
-		book_name = self.clippings_file_handler.readline().strip()
+    def _extract_clip_data(self):
+        note_data = ""
+        note_data_line = self.handler.readline().decode("utf-8").strip()
+        while note_data_line != KindleClippingsGenerator.KINDLE_END_NOTE:
+            note_data = "\n".join([note_data, note_data_line])
+            note_data_line = self.handler.readline().decode("utf-8").strip()
+        return note_data
 
-		if not book_name:
-			self.clippings_file_handler.close()
-			raise StopIteration
+    def __next__(self):
+        try:
+            title = self.handler.readline().decode("utf-8").strip()
+        except ValueError:
+            self.handler.close()
+            raise StopIteration
 
-		meta_data = self.clippings_file_handler.readline().strip().split("|")
+        meta_data = self.handler.readline().decode("utf-8").strip()
 
-		# blank line
-		self.clippings_file_handler.readline().strip()
-		
-		note_data = ""
-		note_data_line = self.clippings_file_handler.readline().strip()
-		while note_data_line != KindleClippingsGenerator.KINDLE_END_NOTE:
-			note_data = "\n".join([note_data, note_data_line])
-			note_data_line = self.clippings_file_handler.readline().strip()
-
-		if len(meta_data) == 1:
-			date_added = meta_data
-			return (book_name, date_added, note_data)
-		elif len(meta_data) == 2:
-			book_location, date_added = meta_data
-			return (book_name, book_location, date_added, note_data)
-		elif len(meta_data) == 3:
-			book_page, book_location, date_added = meta_data
-			return (book_name, book_page, book_location, date_added, note_data)
+        # comment line - useless for parsing
+        self.handler.readline()
+        note_data = self._extract_clip_data()
+        return title, meta_data, note_data
 
 
 def format_clip(clip):
-	try:
-		return "\n".join(clip)
-	except TypeError:
-		import ipdb; ipdb.set_trace()
+    return "\n".join(clip[1:])
 
+def format_title(title_name):
+    for char in title_name:
+        if char not in string.ascii_letters:
+            title_name = title_name.replace(char, "_")
+    return title_name
+
+def is_empty_note(note):
+    return len(note[2]) <= 1
 
 def extract_notes(clippings_file_path, output_notes_dir):
-	for book_name, book_clips in itertools.groupby(KindleClippingsGenerator(clippings_file_path), lambda x: x[0]):
-		book_notes_file_name = "".join([book_name.replace(" ", "_"), "_notes.txt"])
+    for title_name, title_clips in itertools.groupby(KindleClippingsGenerator(clippings_file_path), lambda x: x[0]):
+        title_notes_file_name = "".join([format_title(title_name), "_notes.txt"])
 
-		with open(os.path.join(output_notes_dir, book_notes_file_name), "w+") as current_book_notes:
-			for clip in book_clips:
-				current_book_notes.write(format_clip(clip))
-				current_book_notes.write("\n-----------------\n")
+        title_file_path = os.path.join(output_notes_dir, title_notes_file_name)
+        with open(title_file_path, "w+", encoding="utf-8") as title_file_handler:
+            title_file_handler.write("{} - Notes".format(title_name))
+            for clip in title_clips:
+                if not is_empty_note(clip):
+                    title_file_handler.write(format_clip(clip))
+                    title_file_handler.write("\n\n{}\n\n".format("-" * 100))
+
 
 if __name__ == '__main__':
-	args = docopt.docopt(__doc__)
-	extract_notes(args["<kindle_clipping_file>"], args["<output_dir>"])
+    args = docopt.docopt(__doc__)
+    extract_notes(args["<kindle_clipping_file>"], args["<output_dir>"])
